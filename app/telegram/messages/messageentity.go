199ce10dc83c23f07f10b727b1be038f58ce8f69
Roger Ferrer Ibanez <rofirrim@gmail.com>
2016-11-19 16:41:47 +0000

    Remove Telegram bot infrastructure
    We're going to use a different approach.

--- app/telegram/messages/messageentity.go
+++
@@ -1,9 +0,0 @@
-package messages
-
-type MessageEntity struct {
-	Type   string  `json:"type"`
-	Offset int     `json:"offset"`
-	Length int     `json:"length"`
-	Url    *string `json:"url"`
-	User   *User   `json:"user"`
-}
--- app/telegram/messages/user.go
+++
@@ -1,8 +0,0 @@
-package messages
-
-type User struct {
-	Id        int     `json:"id"`
-	FirstName string  `json:"first_name"`
-	LastName  *string `json:"last_name"`
-	Username  *string `json:"username"`
-}
